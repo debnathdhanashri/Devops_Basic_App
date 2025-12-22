d91a2cbbad3f09a9ed9b91c4aefdd06db31dfee6
Rathijeet <kinjal.boghara@pragmatyc.com>
2025-12-22 17:10:20 +0530

    Name Change

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>🚀 Deployed with AWS CI/CD Pipeline!</h2>
+      <h2>🚀 Deployed with AWS CI/CD Pipeline Rathijeet!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
